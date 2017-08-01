16b8195f1b46a1100c3d35e16105ee310397bcbf
Malachi Burke <malachi.burke@gmail.com>
2017-07-31 19:51:24 -0700

    More simplistic code
    Compiles, not tested

--- src/frab/mbed/i2c.h
+++ src/frab/mbed/i2c.h
@@ -16,6 +16,8 @@ namespace layer2 {
 // TODO: most mbed i2c operations handle address as part of the call
 // either we brute force our way around it, or perhaps it makes more sense
 // to adapt the other i2c frab code to be more address-oriented
+// TODO: make a compile-time-queryable flag denoting whether tx modes or
+// regular bulk modes are preferred
 class i2c_tx
 {
     mbed::I2C& native;
@@ -43,10 +45,27 @@ public:
     {
         native.write(data);
     }
+
+    // TODO: attempt to optimize this, but so far all bulk transfer commands
+    // for mbed take address as a prepended parameter
+    inline void write(const uint8_t* data, size_t length)
+    {
+        while(length--)
+            write(*data++);
+    }
+
+
+    // EXPERIMENTAL
+    inline void addr(uint8_t data, bool is_write_mode = true)
+    {
+        write((data << 1) | (is_write_mode ? 0 : 1));
+    }
 };
 
 class i2c
 {
+    // note mbed::I2C contains virtual functions, so we might consider moving
+    // all this to layer5
     mbed::I2C native;
 
     // FIX: do this instead by pulling in gpio.h, when we can
