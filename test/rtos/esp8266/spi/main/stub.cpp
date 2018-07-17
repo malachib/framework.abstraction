f50ec523ea54f627a66f5e79dbbb152a937ba61e
Malachi Burke <malachi.burke@gmail.com>
2018-07-16 19:23:30 -0700

    Doing init and adding debug print

--- test/rtos/esp8266/spi/main/stub.cpp
+++ test/rtos/esp8266/spi/main/stub.cpp
@@ -1,14 +1,35 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "driver/spi_interface.h"
+#include "driver/spi_register.h"
+
 #include <frab/frab.h>
+
+#include <stdio.h>
 
 void spi_test_1();
 
 extern "C" void test_task(void*)
 {
+    SpiAttr pAttr;   //Set as Master/Sub mode 0 and speed 2MHz
+    pAttr.mode = SpiMode_Master;
+    pAttr.subMode = SpiSubMode_0;
+    pAttr.speed = SpiSpeed_2MHz;
+    // clock divider - SDK seems to do -1 for some reason, so this should
+    // be 1 MHz
+    //pAttr.speed = (SpiSpeed) (80 - 1);
+    pAttr.bitOrder = SpiBitOrder_MSBFirst;
+    // TODO: Change SpiNum_HSPI to this->spi at some point, though
+    // for ESP8266 prtty sure we are mostly interested in the non-flash SPI
+    // (which I *think* actually is the NON HSPI port)
+    SPIInit(SpiNum_HSPI, &pAttr);    
+
+    int debug = 0;
+
     for(;;)
     {
+        printf("Debug: %d\n", debug++);
         vTaskDelay(100);
         spi_test_1();
     }
